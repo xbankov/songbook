abedbea018b950e6bbf4ce54c2cede3ca0a8c696
mikulas <mikulas.bankovic27@gmail.com>
2024-03-13 23:38:27 +0100

    Generate songbook Sort by artist/title Add toc

--- src/build_songbook.py
+++ src/build_songbook.py
@@ -1,16 +1,31 @@
 from dataclasses import dataclass
 from pathlib import Path
-
-import pdfkit
 from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
-
 import config as config
 from helpers import chordpro2html
 
 
+@dataclass
+class SongEntry:
+    title: str
+    artist: str
+    path: str
+
+
+def get_song_entry(path):
+    with open(path, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    soup = BeautifulSoup(content, "html.parser")
+    title, artist = soup.find("h1", {"id": "title"}).get_text(strip=True).split("-")
+    title = title.strip()
+    artist = artist.strip()
+    return SongEntry(title=title, artist=artist, path=path)
+
+
 def main():
-    
+
     print("#####################################################")
     print("######### CONVERT CHORDPRO TO HTML ##################")
     print("#####################################################")
@@ -25,32 +40,35 @@ def main():
         for e in config.HTML_DIR.iterdir()
     ]
 
+    env = Environment(loader=FileSystemLoader(str(config.ROOT_DIR)))
+    book_template = env.get_template(config.TEMPLATE_BOOK)
+
     def sorting_key(song):
         return (song.artist, song.title)
 
     sorted_paths = [song.path for song in sorted(songs, key=sorting_key)]
-
-    env = Environment(loader=FileSystemLoader(str(config.ROOT_DIR)))
-    book_template = env.get_template(config.TEMPLATE_BOOK)
     book_content = book_template.render(songs=sorted_paths)
 
-    with open(config.FINAL_OUTPUT_HTML, "w", encoding="utf-8") as f:
+    with open("songbook.html", "w", encoding="utf-8") as f:
         f.write(book_content)
 
-    configuration = pdfkit.configuration(wkhtmltopdf=str(config.WKHTMLTOPDF))
+    import pdfkit
+
+    configuration = pdfkit.configuration(wkhtmltopdf="/usr/bin/wkhtmltopdf")
     options = {
-        "--footer-center": "[page]",
         "--enable-local-file-access": "",
-        "--outline-depth": 2,
     }
+
+    html_file_path = "songbook.html"
+    pdf_file_path = "songbook.pdf"
     pdfkit.from_file(
-        str(config.FINAL_OUTPUT_HTML),
-        str(config.FINAL_OUTPUT_PDF),
+        str(html_file_path),
+        str(pdf_file_path),
         css=config.TEMPLATE_STYLE,
         configuration=configuration,
         toc={
             "toc-header-text": "Table of Contents",
-            # "xsl-style-sheet": str(config.WKHTMLTOPDF_XSL),
+            
         },
         options=options,
     )
