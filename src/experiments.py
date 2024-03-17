79c70b8247c2c3ca1ecf9b8a45b6a25b63048a78
VladoBaca <asianiajja@gmail.com>
2024-03-17 01:57:40 +0100

    Added parsing from chordpro

--- src/experiments.py
+++ src/experiments.py
@@ -1,14 +1,24 @@
 import config as config
 from helpers import normalize_txt
+from src.model.song import Song
 
 
 def main():
     config.init_dirs()
 
-    raw_file_path = config.RAW_DIR / "tabs.ultimate-guitar.com&tab&elvis-presley&jailhouse-rock-chords-79693.html"
-    normalized_file_path = config.NORMALIZED_DIR / f"jailhouse-rock.txt"
+    #raw_file_path = config.RAW_DIR / "tabs.ultimate-guitar.com&tab&elvis-presley&jailhouse-rock-chords-79693.html"
+    raw_file_path = config.RAW_DIR / "tabs.ultimate-guitar.com&tab&chris-isaak&wicked-game-chords-11066.html"
+    normalized_file_path = config.NORMALIZED_DIR / f"wicked-game.txt"
+    regenerated_file_path = config.NORMALIZED_DIR / f"wicked-game-regenerated.txt"
 
     normalize_txt(raw_file_path, normalized_file_path)
+
+    with open(normalized_file_path, "r", encoding="utf-8") as f:
+        content = f.read()
+    song = Song.parse(content)
+
+    with open(regenerated_file_path, "w", encoding="utf-8") as f:
+        f.write(str(song))
 
 
 if __name__ == "__main__":
--- src/model/song.py
+++ src/model/song.py
@@ -1,3 +1,4 @@
+from src.helpers import get_tag_items, is_tag
 from src.model.composition import Chord
 
 
@@ -12,6 +13,22 @@ class Line:
             in self.parts
         ]
         return "".join(string_parts)
+
+    @staticmethod
+    def parse(text: str):
+        splits = text.split("[")
+        parts = []
+        first_split = splits.pop(0)
+        if first_split:
+            parts.append(first_split)
+
+        for split in splits:
+            sub_splits = split.split("]", 1)
+            parts.append(Chord.parse(sub_splits[0]))
+            if sub_splits[1]:
+                parts.append(sub_splits[1])
+
+        return Line(parts)
 
 
 class Section:
@@ -37,6 +54,28 @@ class Section:
 
         return "\n".join(string_lines)
 
+    @staticmethod
+    def parse(text: str):
+        text_lines = text.split("\n")
+        label = None
+        title = None
+        parsed_lines = []
+
+        for i, line in enumerate(text_lines):
+            if is_tag(line):
+                tag, content = get_tag_items(line)
+
+                if i == 0 and tag.startswith("start_of_"):
+                    label = tag[9:]
+                    if content is not None:
+                        title = content
+                elif label is not None and i == len(text_lines) - 1 and line == f"{{end_of_{label}}}":
+                    pass
+            elif line:
+                parsed_lines.append(Line.parse(line))
+
+        return Section(parsed_lines, label, title)
+
 
 class Song:
     def __init__(self, sections: list[Section], title: str, artist: str, capo: str = None):
@@ -59,3 +98,46 @@ class Song:
             string_sections.append(str(section))
 
         return "\n\n".join(string_sections)
+
+    @staticmethod
+    def parse(text: str):
+        title = None
+        artist = None
+        capo = None
+
+        text_lines = text.split("\n")
+        parsed_sections = []
+        current_section_start = 0
+
+        for i, line in enumerate(text_lines):
+            if is_tag(line):
+                tag, content = get_tag_items(line)
+                current_section_end = i
+                next_section_start = i+1
+
+                if tag.startswith("start_of_"):
+                    next_section_start = i
+                elif tag.startswith("end_of_"):
+                    current_section_end = i+1
+                elif tag == "title":
+                    title = content
+                elif tag == "artist":
+                    artist = content
+                elif tag == "capo":
+                    capo = content
+
+                if current_section_end > current_section_start:
+                    section_lines = text_lines[current_section_start: current_section_end]
+                    parsed_sections.append(Section.parse("\n".join(section_lines)))
+                current_section_start = next_section_start
+            elif not line:
+                if i > current_section_start:
+                    section_lines = text_lines[current_section_start: i]
+                    parsed_sections.append(Section.parse("\n".join(section_lines)))
+                current_section_start = i+1
+
+        if len(text_lines) > current_section_start:
+            section_lines = text_lines[current_section_start:]
+            parsed_sections.append(Section.parse("\n".join(section_lines)))
+
+        return Song(parsed_sections, title, artist, capo)
