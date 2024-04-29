3894e3cd92758fb96c8d225cafab385936a29edd
VladoBaca <asianiajja@gmail.com>
2024-04-29 17:25:41 +0200

    Added new fancy chord schemes, refactored chord scheme parsing, added parsing of chord voicings

--- src/model/composition.py
+++ src/model/composition.py
@@ -23,17 +23,52 @@ intervals = "P1", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m7", "M
 class ChordScheme(Enum):
     major = 0, 4, 7
     minor = 0, 3, 7
+    dom7 = 0, 4, 7, 10
+    maj7 = 0, 4, 7, 11
+    min7 = 0, 3, 7, 10
+    minmaj7 = 0, 3, 7, 11
+    dim = 0, 3, 6
+    aug = 0, 4, 8
+    dim7 = 0, 3, 6, 9
+    min7b5 = 0, 3, 6, 10
+    sus2 = 0, 2, 7
+    sus4 = 0, 5, 7
+    power = 0, 7
+    add4 = 0, 4, 5, 7
+    minadd4 = 0, 3, 5, 7
+    add6 = 0, 4, 7, 9
+    minadd6 = 0, 3, 7, 9
+    maj9 = 0, 2, 4, 7, 11
+    dom9 = 0, 2, 4, 7, 10
 
 
-chord_scheme = {
-    "": ChordScheme.major, "maj": ChordScheme.major, "major": ChordScheme.major,
-    "m": ChordScheme.minor, "min": ChordScheme.minor, "minor": ChordScheme.minor,
+chord_scheme_names = {
+    ChordScheme.major: ["", "maj", "major"],
+    ChordScheme.minor: ["m", "min", "minor"],
+    ChordScheme.dom7: ["7", "dom7"],
+    ChordScheme.maj7: ["Maj7", "major7"],
+    ChordScheme.min7: ["m7", "min7", "minor7"],
+    ChordScheme.minmaj7: ["minMaj7"],
+    ChordScheme.dim: ["dim", "diminished"],
+    ChordScheme.aug: ["aug", "augmented"],
+    ChordScheme.dim7: ["dim7", "diminished7"],
+    ChordScheme.min7b5: ["min7b5", "min7flat5", "half-diminished"],
+    ChordScheme.sus2: ["sus2"],
+    ChordScheme.sus4: ["sus4"],
+    ChordScheme.power: ["5"],
+    ChordScheme.add4: ["add4"],
+    ChordScheme.minadd4: ["minAdd4", "mAdd4"],
+    ChordScheme.add6: ["add6"],
+    ChordScheme.minadd6: ["minAdd6", "mAdd6"],
+    ChordScheme.maj9: ["Maj9"],
+    ChordScheme.dom9: ["dom9"],
 }
 
-chord_scheme_name = {
-    ChordScheme.major: "",
-    ChordScheme.minor: "m",
-}
+chord_scheme_canonical_name = {scheme: chord_names[0] for scheme, chord_names in chord_scheme_names.items()}
+
+chord_name_scheme = {chord_name.lower(): scheme
+                     for scheme, chord_names in chord_scheme_names.items()
+                     for chord_name in chord_names}
 
 
 class Tone(int):
@@ -69,26 +104,36 @@ class Interval(int):
 
 
 class Chord:
-    def __init__(self, root, scheme, scheme_name):
+    def __init__(self, root, scheme, scheme_name, bass=None):
         self.root = root
         self.scheme = scheme
         self.scheme_name = scheme_name
+        self.bass = bass if bass is not None else root
 
     def transpose(self, interval):
-        return Chord(self.root.transpose(interval), self.scheme, self.scheme_name)
+        return Chord(self.root.transpose(interval), self.scheme, self.scheme_name, self.bass.transpose(interval))
 
     @staticmethod
     def parse(label):
+        dash_splits = label.rsplit("/", 1)
+        bass = None
+        if len(dash_splits) > 1:
+            label = dash_splits[0]
+            bass = Tone.parse(dash_splits[1])
         split = 1
         if len(label) > 1 and label[1] in ("#", "b"):
             split = 2
         root = Tone.parse(label[0:split])
         scheme = None
         scheme_name = label[split:]
-        if scheme_name in chord_scheme:
-            scheme = chord_scheme[scheme_name].value
-            scheme_name = chord_scheme_name[chord_scheme[scheme_name]]
-        return Chord(root, scheme, scheme_name)
+        scheme_name_lower = scheme_name.lower()
+        if scheme_name_lower in chord_name_scheme:
+            scheme_id = chord_name_scheme[scheme_name_lower]
+            scheme = scheme_id.value
+            scheme_name = chord_scheme_canonical_name[scheme_id]
+        # TODO VB else warning unknown chord
+        return Chord(root, scheme, scheme_name, bass)
 
     def __str__(self):
-        return f"{self.root}{self.scheme_name}"
+        bass = f"/{self.bass}" if self.bass != self.root else ""
+        return f"{self.root}{self.scheme_name}{bass}"
