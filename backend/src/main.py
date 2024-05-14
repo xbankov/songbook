4cea6e5d9f8a4e19bdc375f3d92ab21170f16f54
mikulas <mikulas.bankovic27@gmail.com>
2024-05-14 23:17:38 +0200

    Backend + Simple frontend prototype

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,14 @@
 from pathlib import Path
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.templating import Jinja2Templates
 from model.song import Song
 
 app = FastAPI(
     title="Songbook API",
-    summary="Retrieve chords for the song from the database or try to download.",
+    summary="Explore internet and create your personal guitar chords songbook to print",
 )
 
 app.add_middleware(
@@ -17,25 +18,26 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.mount("/data", StaticFiles(directory="/data"))
+templates = Jinja2Templates(directory="static")
+path = Path("../../data/after_manual_correction")
 
 
 @app.get("/api/parse/{filename}")
-async def parse(filename):
-    print(__file__)
-    path = Path("../../data/after_manual_correction")
-    print(path / filename)
-    
+async def parse(request: Request, filename):
     with open(path / filename, "r", encoding="utf-8") as f:
         content = f.read()
     song = Song.parse(content)
-    return song.to_json()
-
+    return templates.TemplateResponse("song.html", {"request": request, "song": song})
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root(request: Request):
+    songs = [file.name for file in path.iterdir()]
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "songs": songs}
+    )
 
 
-# Mount the static files directory
-app.mount("/data", StaticFiles(directory="/data"))
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/routes/router";
+import 'bulma/css/bulma.min.css';
 
 const app = createApp(App);
 app.use(router);
