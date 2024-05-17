7ab68f91030e1e76b48dd14bc0411dc517935ab5
mikulas <mikulas.bankovic27@gmail.com>
2024-05-17 14:33:59 +0200

    Remove vue frontend for now

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.css";
-import { createApp } from "vue";
-import App from "@/App.vue";
-import router from "@/routes/router";
-import 'bulma/css/bulma.min.css';
-
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
