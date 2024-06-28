6783aac7b305051a9537ca938cb54d8f12e48338
xbankov <mikulas.bankovic27@gmail.com>
2024-06-28 14:41:58 +0200

    Auth init

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,3 +1,6 @@
+from beanie import Document
+from fastapi_users.db import BeanieBaseUser
+from fastapi_users_db_beanie import BeanieUserDatabase
 from motor.motor_asyncio import AsyncIOMotorClient
 from settings import settings
 from utils import get_logger
@@ -9,8 +12,16 @@ def get_db_client():
     if settings.MONGODB_URI is None:
         logger.error("No database URI provided. Set the MONGODB_URI variable.")
         exit(1)
-    client = AsyncIOMotorClient(settings.MONGODB_URI)
+    client = AsyncIOMotorClient(settings.MONGODB_URI, uuidRepresentation="standard")
     return client
+
+
+class User(BeanieBaseUser, Document):
+    pass
+
+
+async def get_user_db():
+    yield BeanieUserDatabase(User)
 
 
 def init_db():
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from auth import auth_backend, current_active_user, fastapi_users
+from beanie import init_beanie
+from database import User, get_db_client
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from models.users import UserCreate, UserRead, UserUpdate
 from routes import artists, songs
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_beanie(
+        database=get_db_client(),
+        document_models=[
+            User,
+        ],
+    )
+    yield
+
 
 app = FastAPI(
     title="Songbook API",
@@ -15,7 +33,36 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.mount("/static", StaticFiles(directory="static"))
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(songs.router)
 app.include_router(artists.router)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
