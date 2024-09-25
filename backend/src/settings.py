f7e7fcb65e631ab5936e4838abbc786dfab1a336
xbankov <mikulas.bankovic27@gmail.com>
2024-09-25 14:27:33 +0200

    Add Codacy

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -2,6 +2,14 @@ import os
 
 
 class Settings:
+    """
+    Represents the settings for the application.
+
+    Attributes:
+        MONGODB_URI (str | None): The URI for the MongoDB database.
+        Defaults to the value of the environment variable 'MONGODB_URI'.
+    """
+
     MONGODB_URI: str | None = os.getenv("MONGODB_URI")
 
 
