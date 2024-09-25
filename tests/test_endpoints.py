0dda742fd9558c3a9751c53355982ab49e5862e8
xbankov <mikulas.bankovic27@gmail.com>
2024-09-25 16:13:17 +0200

    Add after-build tests

---
+++ tests/test_endpoints.py
@@ -0,0 +1,7 @@
+import requests
+
+
+def test_index():
+    response = requests.get("http:/localhost:8000/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to FastAPI"}
