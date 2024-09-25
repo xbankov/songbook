48b638aec10325d402a96b26ed12e8f343895c05
xbankov <mikulas.bankovic27@gmail.com>
2024-09-25 16:31:23 +0200

    Fix single slash in url

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_index():
-    response = requests.get("http:/localhost:8000/")
+    response = requests.get("http://localhost:8000/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to FastAPI"}
