703ff369663123617261453482e7e60703c866f5
vladimir-baca <vladimir.baca@themama.ai>
2024-10-05 21:11:54 +0200

    formatting

--- backend/src/models/composition.py
+++ backend/src/models/composition.py
@@ -4,17 +4,24 @@ from pydantic import BaseModel, constr
 
 tones: dict[str, int] = {
     "C": 0,
-    "C#": 1, "Db": 1,
+    "C#": 1,
+    "Db": 1,
     "D": 2,
-    "D#": 3, "Eb": 3,
+    "D#": 3,
+    "Eb": 3,
     "E": 4,
     "F": 5,
-    "F#": 6, "Gb": 6,
+    "F#": 6,
+    "Gb": 6,
     "G": 7,
-    "G#": 8, "Ab": 8,
+    "G#": 8,
+    "Ab": 8,
     "A": 9,
-    "A#": 10, "Bb": 10, "Hb": 10,
-    "B": 11, "H": 11,
+    "A#": 10,
+    "Bb": 10,
+    "Hb": 10,
+    "B": 11,
+    "H": 11,
 }
 
 chromatic_scale = "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
